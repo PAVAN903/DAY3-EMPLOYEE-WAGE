8953ef02e63d725d39191b3b6a0dc4630ce17570
PAVAN903 <navap903@gmail.com>
2023-02-22 10:40:06 +0530

    added:employee part time wage

--- src/com/bridgelab/EmployeeWage.java
+++ src/com/bridgelab/EmployeeWage.java
@@ -6,11 +6,16 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		Random ran = new Random();
-		int attendance = ran.nextInt(2);
+		int attendance = ran.nextInt(3);
 		int WageperHour=20;
 		int FullDayHour=8;
+		int PartTimeHour=4;
 		if (attendance == 0) {
 			System.out.println("employee is absent");
+		}else if(attendance==1) {
+			System.out.println("employee is present but part time");
+			int PartTimeWage=PartTimeHour*WageperHour;
+			System.out.println("Employee part time wage is:: "+PartTimeWage);
 		} else {
 			System.out.println("employee is present");
 			int DailyWage=FullDayHour*WageperHour;
