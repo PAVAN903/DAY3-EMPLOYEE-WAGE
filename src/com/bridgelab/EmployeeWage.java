f51957227264afca8cba1c2cd945b144ffc2295f
PAVAN903 <navap903@gmail.com>
2023-02-22 10:33:30 +0530

    Added:employee daily wage

--- src/com/bridgelab/EmployeeWage.java
+++ src/com/bridgelab/EmployeeWage.java
@@ -7,10 +7,14 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		Random ran = new Random();
 		int attendance = ran.nextInt(2);
+		int WageperHour=20;
+		int FullDayHour=8;
 		if (attendance == 0) {
 			System.out.println("employee is absent");
 		} else {
 			System.out.println("employee is present");
+			int DailyWage=FullDayHour*WageperHour;
+			System.out.println("Daily employee wage is:: "+DailyWage);
 		}
 
 	}
